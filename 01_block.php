d98dd18c1abf9a5e6c07bb5bb3da92a199ec602f
juan manuel <jmgv87191@gmail.com>
2024-09-24 13:35:59 -0700

    asd

---
+++ 01_block.php
@@ -0,0 +1,12 @@
+<?php 
+  include_once './includes/header.php'; 
+?>
+
+<?php
+
+  echo "Hola mundo";
+
+  var_dump( 'asdasd22')
+
+
+?>
---
+++ 02_echo.php
@@ -0,0 +1,15 @@
+<?php 
+  include_once './includes/header.php'; 
+?>
+
+<?php
+
+
+  echo "Hola mundo";
+  echo "<br>";
+  echo 20;
+
+
+?>
+
+<br> 
