1408092e64c9ea7aae444ad0e7b52da711be53c3
juan manuel <jmgv87191@gmail.com>
2024-09-24 14:46:47 -0700

    asd

--- 05_arrays.php
+++ 05_arrays.php
@@ -1,3 +1,28 @@
 <?php 
   include_once './includes/header.php'; 
 ?>
+
+
+<?php
+
+  $frutas = ['manzana','pera'];
+
+  echo "<pre>";
+
+    var_dump($frutas);
+
+  echo "</pre>";
+
+  echo "//////////////////////////////////////////////////////////";
+
+  array_push( $frutas, 'melon' );
+  
+  unset( $frutas[1] );
+
+  echo "<pre>";
+
+    var_dump( $frutas );
+
+  echo "</pre>";
+
+?>
