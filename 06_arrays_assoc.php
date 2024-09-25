6bb6a0f53e9c5974bcb1b36655ecce55e6397c53
jmgv87191 <jmgv87191@gmail.com>
2024-09-24 18:01:02 -0700

    asd

--- 06_arrays_assoc.php
+++ 06_arrays_assoc.php
@@ -1,3 +1,24 @@
 <?php 
   include_once './includes/header.php'; 
 ?>
+
+<?php
+
+  $estudiante = [
+
+    'name' => 'juan',
+    'edad' => 35,
+    
+  ];
+
+  echo '<pre>';
+
+  var_dump($estudiante);
+
+  echo '</pre>';
+  
+  echo $estudiante ['name'] . " ". $estudiante['edad']
+
+
+
+?>
