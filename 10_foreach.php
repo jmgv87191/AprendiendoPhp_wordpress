fe4597f7dfb13295b9579c9117cc8fe963815177
jmgv87191 <jmgv87191@gmail.com>
2024-09-24 20:12:54 -0700

    asd

--- 10_foreach.php
+++ 10_foreach.php
@@ -1,3 +1,17 @@
 <?php 
   include_once './includes/header.php'; 
 ?>
+
+
+<?php
+
+
+$frutas = ['manzana','pera','mango'];
+
+foreach ($frutas as $fruta => $value) {
+
+  echo $value."<br>";
+
+}
+
+?>
